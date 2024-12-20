09756957ef5d02bc0aeeb2f164ddbb1987ce6736
Sedrowow <55648914+Sedrowow@users.noreply.github.com>
2024-12-20 11:34:37 +0100

    Update script.lua

--- MAL/script.lua
+++ MAL/script.lua
@@ -327,7 +327,7 @@ function announceGroupSpawn(group_id, peer_id)
     message = message .. "Group ID: " .. group_id .. "\n"
     message = message .. "Total Lag Cost: " .. total_lag_cost
 
-    server.announce("[MAL]", message)
+    server.notify("[MAL]", message, 4)
 
     -- Determine lag cost limit based on whether it's a workshop vehicle
     local lag_cost_limit = PLAYER_LAG_COST_LIMIT
