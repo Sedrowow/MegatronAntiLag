5f64f0519e7c2dec2a0cc76f300aad5a6d4a47e9
Sedrowow <55648914+Sedrowow@users.noreply.github.com>
2024-12-20 10:18:28 +0100

    Update script.lua of RULS
    fixes that i forgot to add are now implemented

--- RULS/script.lua
+++ RULS/script.lua
@@ -1,10 +1,10 @@
-local discord = "mayhemindustries"
+local discord = "4EqNtG9ZGX"
 local systemName = "[Auth System]"
 local rules = {
     "===RULES===",
     "1) Please use common sense and stop doing a certain action when told to.",
     "2) Be Friendly! There is no need to explain that!",
-    "3) Vehicle that cause many warning messages as well as laggy vehicles are restricted!",
+    "3) Vehicle that cause more than two warning messages as well as laggy vehicles are prohibited!",
     "4) Workshop is allowed, as long as they fit into the rules!.",
     "5) PvP in Hangars or spawn areas are not allowed, Please move",
     "6) Please Refrain from using Flare Bombs, EMPs or anything to annoy people.",
@@ -34,7 +34,7 @@ local player_ui_ids = {}
 
 function onPlayerJoin(steam_id, name, user_id, admin, auth)
     player_ui_ids[user_id] = 1
-    local ui_id = user_id * 100 + player_ui_ids[user_id]
+    ui_id = user_id * 100 + player_ui_ids[user_id]
     if (auth) and not (admin) then
         server.removeAuth(user_id)
         showPopup(user_id, ui_id, "To get authed\ntype ?rules")
@@ -53,7 +53,7 @@ end
 function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one, two, three, four, five)
     command = string.lower(command)
     player_ui_ids[peer_id] = (player_ui_ids[peer_id] or 0) + 1
-    local ui_id = peer_id * 100 + player_ui_ids[peer_id]
+    ui_id = peer_id * 100 + player_ui_ids[peer_id]
 
     if (command == "?rules") then
         local rules = createList(
@@ -64,12 +64,12 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
         announce(rules, peer_id)
         removePopup(peer_id, ui_id)
         player_ui_ids[peer_id] = player_ui_ids[peer_id] + 1
-        ui_id = peer_id * 100 + player_ui_ids[peer_id]
-        showPopup(peer_id, ui_id, "To get auth, type ?auth "..peer_id.." 'accept'")
+        ui_id2 = peer_id * 100 + player_ui_ids[peer_id]
+        showPopup(peer_id, ui_id2, "To get auth, type in one line:\n?auth\n"..peer_id.."\naccept")
     end
     if (command == "?auth") then
         if (one == nil or two == nil) then
-            announce("Usage: ?auth player_id 'accept'", peer_id)
+            announce("Usage: ?auth player_id accept", peer_id)
             return
         end
 
@@ -84,7 +84,7 @@ function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one,
             if (peer_id == peerID) then
                 server.addAuth(peer_id)
                 announce("You have been granted auth", peer_id)
-                removePopup(peer_id, ui_id)
+                removePopup(peer_id, ui_id2)
             else
                 announce("That is not your player_id! Your player id is: '"..peer_id.."'!", peer_id)
             end
