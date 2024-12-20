192111359c0e916ac551b09311c42d0759091ebd
Sedrowow <55648914+Sedrowow@users.noreply.github.com>
2024-12-20 10:12:27 +0100

    added "RULS" & "vote" script
    a side script for displaying rules and a side script for voting

---
+++ RULS/script.lua
@@ -0,0 +1,146 @@
+local discord = "mayhemindustries"
+local systemName = "[Auth System]"
+local rules = {
+    "===RULES===",
+    "1) Please use common sense and stop doing a certain action when told to.",
+    "2) Be Friendly! There is no need to explain that!",
+    "3) Vehicle that cause many warning messages as well as laggy vehicles are restricted!",
+    "4) Workshop is allowed, as long as they fit into the rules!.",
+    "5) PvP in Hangars or spawn areas are not allowed, Please move",
+    "6) Please Refrain from using Flare Bombs, EMPs or anything to annoy people.",
+    "7) Do NOT at all try break the server in any way (this will lead to a perminent ban!)",
+    "9) Ensure you are given permission by the other player to PvP",
+    "10) Admins have final say",
+    "",
+    "And dont forget to have fun!"
+}
+
+local randomAnnouncements = {
+    string.format("For support and updates\nconsider joining our Discord!\ndiscord.gg/" .. discord),
+    "Nuclear bombs (if with bad intentions or at spawn), Flare Bombs, Nuclear reactors designed to explode or anything designed to annoy or break the server are not permitted",
+    "Got desynced from a Server?\nPress the F2 key to enable\nthe Vehicle Debugger which helps\nwith catching up to servers!",
+    "You have the ability to vote!, to see what is avaiable just type: '?votehelp' !"
+}
+
+local function showPopup(peer_id, ui_id, text)
+    server.setPopupScreen(peer_id, ui_id, "[AuthSys]", true, text, 0, 0)
+end
+
+local function removePopup(peer_id, ui_id)
+    server.removePopup(peer_id, ui_id)
+end
+
+local player_ui_ids = {}
+
+function onPlayerJoin(steam_id, name, user_id, admin, auth)
+    player_ui_ids[user_id] = 1
+    local ui_id = user_id * 100 + player_ui_ids[user_id]
+    if (auth) and not (admin) then
+        server.removeAuth(user_id)
+        showPopup(user_id, ui_id, "To get authed\ntype ?rules")
+        announce("To get authed again, type ?rules", user_id)
+    elseif not (auth) and not (admin) then
+        showPopup(user_id, ui_id, "To get authed\ntype ?rules")
+        announce("To get authed, type ?rules", user_id)
+    end
+    if (admin) then
+        if not (auth) then
+            server.addAuth(user_id)
+        end
+    end
+end
+
+function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, one, two, three, four, five)
+    command = string.lower(command)
+    player_ui_ids[peer_id] = (player_ui_ids[peer_id] or 0) + 1
+    local ui_id = peer_id * 100 + player_ui_ids[peer_id]
+
+    if (command == "?rules") then
+        local rules = createList(
+        rules, 
+        function(rules) return true end, 
+        function(rules) return rules end
+    )
+        announce(rules, peer_id)
+        removePopup(peer_id, ui_id)
+        player_ui_ids[peer_id] = player_ui_ids[peer_id] + 1
+        ui_id = peer_id * 100 + player_ui_ids[peer_id]
+        showPopup(peer_id, ui_id, "To get auth, type ?auth "..peer_id.." 'accept'")
+    end
+    if (command == "?auth") then
+        if (one == nil or two == nil) then
+            announce("Usage: ?auth player_id 'accept'", peer_id)
+            return
+        end
+
+        local peerID = tonumber(one)
+
+        if (peerID == nil) then
+            announce("Invalid player ID", peer_id)
+            return
+        end
+
+        if one ~= nil and two == "accept" then
+            if (peer_id == peerID) then
+                server.addAuth(peer_id)
+                announce("You have been granted auth", peer_id)
+                removePopup(peer_id, ui_id)
+            else
+                announce("That is not your player_id! Your player id is: '"..peer_id.."'!", peer_id)
+            end
+        end
+    end
+end
+
+local messagesBeforeRandomAnnounce = 0
+
+function onChatMessage(peer_id, sender_name, message)
+    messagesBeforeRandomAnnounce = messagesBeforeRandomAnnounce + 1
+    if (messagesBeforeRandomAnnounce > 10) then
+        messagesBeforeRandomAnnounce = 0
+        local randInt = math.random(1, #randomAnnouncements)
+        announce(randomAnnouncements[randInt])
+    end
+end
+
+function createList(items, condition, transform)
+    local result = ""
+    local hasItems = false
+
+    for i, item in ipairs(items) do
+        if condition(item) then
+            if transform then
+                result = result .. transform(item)
+            else
+                result = result .. tostring(item)
+            end
+
+            local nextItemIndex = i + 1
+            while nextItemIndex <= #items and not condition(items[nextItemIndex]) do
+                nextItemIndex = nextItemIndex + 1
+            end
+
+            if nextItemIndex <= #items then
+                result = result .. "\n"
+            end
+
+            hasItems = true
+        end
+    end
+
+    if not hasItems then
+        result = "NONE"
+    end
+
+    return result
+end
+
+function announce(message, peerID)
+    -- Insert "|" before each newline in the message
+    message = message:gsub("\n", "\n| ")
+    if peerID then
+        server.announce(systemName, "| " .. message, peerID)
+    else
+        server.announce(systemName, "| " .. message)
+    end
+end
---
+++ vote/script.lua
@@ -0,0 +1,261 @@
+local commands = {	
+	"===Vote Commands (pg 4/4)===",
+	"?vote yes/no  for a apporval or disapporoval of the vote",
+	"?vote kick user_id - vote kick someone out",
+	"?vote disaster - vote for a random disatser",
+	"?vote text (full vote message) - do a custom vote, just put in your vote after 'text ' and youre good to go!"
+}
+
+
+function onCreate(is_world_create)
+	votingdata = {}
+	hasVoteddata  = {}
+	
+	voteinprogress = false
+	playercount = 0
+	minrequirement = 0
+	
+	tick = 0
+	
+	votetarget = nil
+	voteID = 0
+	votetext = nil
+end
+
+minVoteKickRequirement = 0.75
+minVoteDistasterRequirement = 0.5
+
+votetime = 3000
+
+function clamp(a,b,c)
+	return math.min(math.max(a,b),c)
+end
+
+function noVote(P_ID)
+	server.notify(P_ID,"[VOTE]","There is no vote in progress!",8)
+end
+
+function AlreadyVoted(P_ID)
+	server.notify(P_ID,"[VOTE]","You already voted!",8)
+end
+
+function VoteInProgress(P_ID)
+	server.notify(P_ID,"[VOTE]","There is already a vote in progress!",8)
+end
+
+function isVoteInProgress()
+	if voteinprogress == true then
+		return true 
+	else
+		return false
+	end
+end
+
+function tableLength(tab)
+	local length = 0
+	for i in pairs(tab) do
+		length = length + 1
+	end
+	return length
+end
+
+function pickRandomDisaster(id)
+	local random = math.floor(math.random(0,5))
+	local intensity = math.random(0,1)
+	x1 = math.random(0,1000)-500
+	y1 = math.random(0,1000)-500
+	z1 = math.random(0,1000)-500
+	transform_matrix, is_success = server.getPlayerPos(peer_id)
+	x,y,z = matrix.position(transform_matrix)
+	local matrix = matrix.translation(x1+x,y1+y,z1+z)
+	if random == 0 then server.spawnTsunami(matrix,intensity) end
+	if random == 1 then server.spawnWhirlpool(matrix,intensity) end
+	if random == 2 then server.spawnTornado(matrix, 0.5) end
+	if random == 3 then server.spawnMeteorShower(matrix,intensity,true) end
+	if random == 4 then server.spawnMeteor(matrix,intensity) end
+end
+
+function voteSuccessfull()
+	server.notify(-1,"[VOTE]","Vote was successful",8)
+	if voteID == 1 then
+		server.kickPlayer(votetarget)
+	end	
+	if voteID == 2 then
+		pickRandomDisaster(votetarget)
+	end
+	if voteID == 3 then
+		server.cleanVehicles()
+		server.announce("[VOTE]", "Server has been cleaned up by vote! Sorry for any inconvenience caused.")
+	end
+	if voteID == 4 then
+	    server.announce("[VOTE]", "succesful Vote: "..votetext)
+	end
+end
+
+function voteStart(minReq,votetype)
+	voteinprogress = true
+	minrequirement = minReq
+	voteID = votetype
+	playercount = tableLength(server.getPlayers())-1
+end
+
+function voteEnd()
+	votingdata = {}
+	hasVoteddata = {}
+	voteinprogress = false
+end
+
+function getRatio()
+	return #votingdata/playercount
+end
+
+function onTick(game_ticks)
+	
+	if isVoteInProgress() then
+		if getRatio() >= 1 then
+			voteSuccessfull()
+			voteEnd()
+		end
+		
+		tick = tick+game_ticks
+		if tick >= votetime then
+			tick = 0
+			if getRatio() < minrequirement then
+				server.notify(-1,"[VOTE]","Vote was unsuccessful",8)
+			else
+				voteSuccessfull()
+			end
+			voteEnd()
+		end
+	end
+end
+
+function cNameStr(id)
+	return id..": "..server.getPlayerName(id)
+end
+
+function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command, arg1, arg2)
+	if command=="?delobjs" and is_auth then
+		count=0
+		for i=1,99999 do
+			is_success = server.despawnObject(i, true)
+			if is_success then count=count+1 end
+		end
+		server.notify(user_id, "server-command", "removed "..tostring(count).." items/objects", 7)
+	end
+	if (command == "?votehelp") then
+            local commands = createList(
+            commands, 
+            function(commands) return true end, 
+            function(commands) return help end
+        )
+		announce(help, peer_id)
+        end
+	if command == "?vote" then
+		
+		local Voted = hasVoteddata[user_peer_id]
+		if Voted == nil then Voted = false end
+				
+		if arg1 == "yes" then
+			if isVoteInProgress() then
+				if Voted == false then
+					hasVoteddata[user_peer_id] = true
+					votingdata[#votingdata+1] = true
+					server.notify(-1,"[VOTE]", cNameStr(user_peer_id).." Voted yes "..#votingdata.."/"..playercount,8)
+				else
+					AlreadyVoted(user_peer_id)
+				end
+			else
+				noVote(user_peer_id)
+			end
+		end
+		
+		if arg1 == "no" then
+			if isVoteInProgress() then
+				if Voted == false then
+					hasVoteddata[user_peer_id] = true
+					server.notify(-1,"[VOTE]", cNameStr(user_peer_id).." Voted no "..#votingdata.."/"..playercount,8)
+				else
+					AlreadyVoted(user_peer_id)
+				end
+			else
+				noVote(user_peer_id)
+			end
+		end
+
+		
+		if arg1 == "kick" then
+			if not isVoteInProgress() then
+				voteStart(minVoteKickRequirement,1)
+				server.announce("[VOTE]","Started kick vote against: "..arg2.." : "..server.getPlayerName(arg2)..". '?vote yes' to agree, or '?vote no' to disagree!")
+				server.notify(-1,"[VOTE]", cNameStr(user_peer_id).." Started a votekick againts "..arg2.." : "..server.getPlayerName(arg2),8)
+				votetarget = arg2
+			else
+				VoteInProgress(user_peer_id)
+			end
+		end
+		
+		if arg1 == "disaster" then
+			if not isVoteInProgress() then
+				voteStart(minVoteDistasterRequirement,2)
+				server.announce("[VOTE]","Started disaster vote, '?vote yes' to agree, or '?vote no' to disagree!")
+				server.notify(-1,"[VOTE]", cNameStr(user_peer_id).." Started a vote for a random distaster",8)
+				votetarget = user_peer_id
+			else
+				VoteInProgress(user_peer_id)
+			end
+		end
+		if arg1 == "cleanup" then
+			if not isVoteInProgress() then
+				voteStart(0.6,3)
+				server.announce("[VOTE]","Started cleanup vote, '?vote yes' to agree, or '?vote no' to disagree!")
+				server.notify(-1,"[VOTE]", cNameStr(user_peer_id).." Started a vote for cleanup",8)
+				votetarget = user_peer_id
+			else
+				VoteInProgress(user_peer_id)
+			end
+		end
+		if arg1 == "text" then
+		    if not isVoteInProgress() then
+		        voteStart(0.5, 4)
+		        full_message = full_message:gsub("%^?vote text ","")
+		        full_message = full_message:gsub("%?","")
+		        server.announce("[VOTE]","Started text Vote, '?vote yes' to agree, or '?vote no' to disagree!")
+				server.notify(-1,"[VOTE]", cNameStr(user_peer_id).." Started vote: \n"..full_message,8)
+				votetext = full_message
+		    else
+		        VoteInProgress(user_peer_id)
+            end
+        end
+		if arg1 == "vevo" and is_admin then
+			if isVoteInProgress() then
+				voteEnd()
+				server.notify(-1,"[VOTE]","Vote was halted!",8)
+			else
+				noVote(user_peer_id)
+			end
+		end
+		
+	end
+	
+	if is_admin then	
+		if command == "?min-kick-req" then
+			val = clamp(arg1,0,1)
+			minVoteKickRequirement = val 
+			server.notify(user_peer_id,"[VOTE]","Minimum vote-kick requirement changed to "..(val *100).."%",8)
+		end
+			
+		if command == "?min-disaster-req" then
+			val = clamp(arg1,0,1)
+			minVoteDistasterRequirement = val
+			server.notify(user_peer_id,"[VOTE]","Minimum vote disaster requirement changed to "..(val*100).."%",8)
+		end
+		
+		if command == "?timer-length" then
+			val = math.max(arg1,0)
+			votetime = val*60
+			server.notify(-1,"[VOTE]","Vote timer set to "..val.." seconds",8)
+		end
+	end
+
+end
